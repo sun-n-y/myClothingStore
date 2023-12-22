74b6508f996500859c4102e66f51696836d9ceac
sunny patel <sunnykpatel.skp@gmail.com>
2023-12-22 12:04:27 -0500

    add navbar component fixes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,12 +7,14 @@ const Navbar = () => {
     <nav className="bg-base-200">
       <div className="navbar align-elements">
         <div className="navbar-start">
+          {/* TITLE */}
           <NavLink
             to="/"
             className="hidden lg:flex btn btn-primary text-3xl items-center"
           >
             S
           </NavLink>
+          {/* DROPDOWN */}
           <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
               <FaBarsStaggered className="h-6 w-6" />
@@ -21,13 +23,17 @@ const Navbar = () => {
               tabIndex={0}
               className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-200 rounded-box w-52"
             >
-              links
+              nav links
             </ul>
           </div>
         </div>
-        <div className="navbar-center hidden lg:flex">center</div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal">center links</ul>
+        </div>
         <div className="navbar-end">
-          <NavLink to="/cart" className="btn btn-ghost btn-circle btn-md ml-4">
+          {/* THEME ICONS */}
+          {/* CART LINK */}
+          <NavLink to="cart" className="btn btn-ghost btn-circle btn-md ml-4">
             <div className="indicator">
               <BsCart3 className="h-6 w-6" />
               <span className="badge badge-sm badge-primary indicator-item">

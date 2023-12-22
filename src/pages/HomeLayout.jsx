2c728da8ac2511a53e7716eea3308990aebd7932
sunny patel <sunnykpatel.skp@gmail.com>
2023-12-21 19:45:50 -0500

    add header component

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from 'react-router-dom';
+import Header from '../components/Header';
 
 const HomeLayout = () => {
   return (
     <>
+      <Header />
       <nav className="font-bold">navbar</nav>
       <section className="align-elements py-20">
         <Outlet />

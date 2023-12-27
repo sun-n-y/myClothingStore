ce274753e7c60c41da50b87478482a3457ec51fc
sunny patel <sunnykpatel.skp@gmail.com>
2023-12-27 16:16:11 -0500

    add checkbox input

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -2,6 +2,7 @@ import { Form, Link, useLoaderData } from 'react-router-dom';
 import FormInput from './FormInput';
 import FormSelect from './FormSelect';
 import FormRange from './FormRange';
+import FormCheckbox from './FormCheckbox';
 
 const Filters = () => {
   const { meta } = useLoaderData();
@@ -39,6 +40,7 @@ const Filters = () => {
       {/* RANGE */}
       <FormRange label="Select Price Range" name="price" size="range-sm" />
       {/* CHECKBOX */}
+      <FormCheckbox label="free shipping" name="shipping" size="checkbox-sm" />
       {/* BUTTONS */}
       <button type="submit" className="btn btn-primary btn-sm">
         Search

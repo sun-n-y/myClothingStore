bffbef42a2b0a0518d3c9a0224b9c5a488c9e773
sunny patel <sunnykpatel.skp@gmail.com>
2023-12-29 12:38:33 -0500

    add pagination checkpoint

--- src/components/PaginationContainer.jsx
+++ src/components/PaginationContainer.jsx
@@ -1,5 +1,60 @@
+import { useLoaderData, useLocation, useNavigate } from 'react-router-dom';
+
 const PaginationContainer = () => {
-  return <div>PaginationContainer</div>;
+  const { meta } = useLoaderData();
+  const { pageCount, page } = meta.pagination;
+  const { search, pathname } = useLocation();
+  const navigate = useNavigate();
+
+  const pages = Array.from({ length: pageCount }, (_, index) => {
+    return index + 1;
+  });
+
+  const handlePageChange = (pageNumber) => {
+    const searchParams = new URLSearchParams(search);
+    searchParams.set('page', pageNumber);
+    navigate(`${pathname}?${searchParams.toString()}`);
+  };
+
+  if (pageCount < 2) return null;
+
+  return (
+    <div className="mt-16 flex justify-end">
+      <div className="join">
+        <button
+          className="btn btn-xs sm:btn-md join-item"
+          onClick={() => {
+            handlePageChange('prev');
+          }}
+        >
+          Prev
+        </button>
+        {pages.map((pageNumber) => {
+          return (
+            <button
+              key={pageNumber}
+              className={`btn btn-xs sm:btn-md border-none join-item ${
+                pageNumber === page && 'bg-base-300 border-base-300'
+              }`}
+              onClick={() => {
+                handlePageChange(pageNumber);
+              }}
+            >
+              {pageNumber}
+            </button>
+          );
+        })}
+        <button
+          className="btn btn-xs sm:btn-md join-item"
+          onClick={() => {
+            handlePageChange('next');
+          }}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default PaginationContainer;

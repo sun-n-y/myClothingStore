9861f40aad33a676fa55c27cd8b07d5e6160d493
sunny patel <sunnykpatel.skp@gmail.com>
2024-01-04 15:51:36 -0500

    add dispatch to cart page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -3,7 +3,8 @@ import { CartItemsList, SectionTitle, CartTotals } from '../components';
 import { Link } from 'react-router-dom';
 
 const Cart = () => {
-  const user = null;
+  const user = useSelector((state) => state.userState.user);
+
   const { numItemsInCart } = useSelector((state) => state.cart);
 
   if (numItemsInCart === 0) return <SectionTitle text="Your cart is empty" />;

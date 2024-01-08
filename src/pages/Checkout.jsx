e0b5950c9a80e3bf95b13ba81c57b5959eb4f839
sunny patel <sunnykpatel.skp@gmail.com>
2024-01-08 12:16:46 -0500

    add checkout route bug fix

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -4,7 +4,8 @@ import { toast } from 'react-toastify';
 import { redirect } from 'react-router-dom';
 
 export const loader = (store) => async () => {
-  const user = store.getState().user;
+  const user = store.getState().userState;
+  console.log(user);
   if (!user) {
     toast.warning('You must be logged in to checkout');
     return redirect('/login');
